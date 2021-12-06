5d61af7757d6f3e67c91bd31c7ad6c61dbb18b3e
dhs02 <59893400+dhs02@users.noreply.github.com>
2021-12-06 21:32:39 +0100

    JS Basics exercise

---
+++ 03_js_basics/01_first_js/script.js
@@ -0,0 +1,20 @@
+// Step 4
+console.log("Hello Winc Academy");
+// Step 5
+let name = 'Dave';
+console.log(name);
+// Step 7
+console.log(4 + 4);
+console.log('4' + '4')
+// Step 8
+console.log(10 + 10);
+console.log(10 - 5);
+console.log(10 * 10);
+console.log(20 / 2);
+console.log(10 % 3);
+// Step 9
+let age = 31;
+console.log(typeof age);
+// Step 10
+age = '31';
+console.log(typeof age);
