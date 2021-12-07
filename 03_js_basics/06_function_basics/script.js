fe9983e8db30376089888bcdca564622055b5226
dhs02 <59893400+dhs02@users.noreply.github.com>
2021-12-07 19:42:00 +0100

    Function basics

---
+++ 03_js_basics/06_function_basics/script.js
@@ -0,0 +1,19 @@
+const makePizza = function () {
+    console.log("Make pizza crust");
+    console.log("Put ingredients on pizza crust");
+    console.log("Put pizza in oven");
+};
+
+const makeSushi = function () {
+    console.log("Cook rice");
+    console.log("Add layer of rice to nori sheet");
+    console.log("Add additional ingredients");
+    console.log("Make sushi roll");
+    console.log("Slice sushi roll");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
